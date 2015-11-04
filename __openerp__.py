a8af24899b480b9dd36eac043c4e13c79f867389
Atul Jain <jain.atul10@hotmail.com>
2015-11-04 20:57:53 +0530

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+
+{
+	'name': 'Procurement hide ',
+	'category': 'General',
+	'version':"1.0",
+	'author':"Atul Jain<jain.atul43@gmail.com>",
+	'depends':['base','sale', 'product'],
+	'sequence':1,
+	'description': """
+This module will hide procurement tab for particular user group:
+""",
+	'data':[
+		'product.xml'
+
+	],
+	'installable':True,
+	'auto_install':False
+
+}
